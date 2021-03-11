973e9d1ebfcbc1fbd6024d25bc3f81dd09542388
Tyler Chase <tylercchase@gmail.com>
2021-03-10 17:18:00 -0900

    Send json

--- client.js
+++ client.js
@@ -1,10 +1,11 @@
 var udp = require('dgram');
 
 var buffer = require('buffer');
+const { json } = require('express');
 
 // creating a client socket
 var client = udp.createSocket('udp4');
-
+let serverAddress = 'localhost'//'192.168.1.102'
 //buffer msg
 var data = Buffer.from('siddheshrane');
 
@@ -14,7 +15,7 @@ client.on('message',function(msg,info){
 });
 
 //sending msg
-client.send(data,2222,'localhost',function(error){
+client.send(data,2222,serverAddress,function(error){
   if(error){
     client.close();
   }else{
@@ -26,7 +27,16 @@ var data1 = Buffer.from('hello');
 var data2 = Buffer.from('world');
 
 //sending multiple msg
-client.send([data1,data2],2222,'localhost',function(error){
+client.send([data1,data2],2222,serverAddress,function(error){
+  if(error){
+    client.close();
+  }else{
+    console.log('Data sent !!!');
+  }
+});
+
+let datajson = Buffer.from(JSON.stringify({test: 'hi'}))
+client.send(datajson,2222,serverAddress,function(error){
   if(error){
     client.close();
   }else{
