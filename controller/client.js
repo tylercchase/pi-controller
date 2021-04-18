ce7e7ede142e156f34d2d65ef1c7bf943e1aca19
Tyler Chase <tylercchase@gmail.com>
2021-04-17 22:35:58 -0800

    Readd button logic

--- controller/client.js
+++ controller/client.js
@@ -2,49 +2,49 @@ let udp = require('dgram');
 
 let buffer = require('buffer');
 const { json } = require('express');
-// let Gpio = require('onoff').Gpio;
+let Gpio = require('onoff').Gpio;
 // // creating a client socket
 var client = udp.createSocket('udp4');
 let serverAddress = '192.168.1.118'
 
-// let buttonA = new Gpio(14, 'in', 'both')
+let buttonA = new Gpio(14, 'in', 'both')
 
-// function errorChecking(err) {
-//   if(err) {
-//     console.error(err)
-//   }
-//   else {
-//     console.log('Data sent!')
+function errorChecking(err) {
+  if(err) {
+    console.error(err)
+  }
+  else {
+    console.log('Data sent!')
 
-//   }
-// }
+  }
+}
 
-// buttonA.watch(function(err, _value) {
-//   if(err) {
-//     console.error(`There was an error ${err}`)
-//   } else {
-//     client.send(Buffer.from(JSON.stringify({button:1,value: value})),
-//     2222,serverAddress,errorChecking)
-//   }
-// })
+buttonA.watch(function(err, _value) {
+  if(err) {
+    console.error(`There was an error ${err}`)
+  } else {
+    client.send(Buffer.from(JSON.stringify({button:1,value: value})),
+    2222,serverAddress,errorChecking)
+  }
+})
 client.on('message',function(msg,info){
   console.log('Data received from server : ' + msg.toString());
   console.log('Received %d bytes from %s:%d\n',msg.length, info.address, info.port);
 });
 
-let datajson = Buffer.from(JSON.stringify({test: 'hi'}))
-client.send(datajson,2222,serverAddress,function(error){
-  if(error){
-    client.close();
-  }else{
-    console.log('Data sent !!!');
-  }
-});
+// let datajson = Buffer.from(JSON.stringify({test: 'hi'}))
+// client.send(datajson,2222,serverAddress,function(error){
+//   if(error){
+//     client.close();
+//   }else{
+//     console.log('Data sent !!!');
+//   }
+// });
 
 
 
-// function unexportOnClose() { //function to run when exiting program
-//   buttonA.unexport(); // Unexport Button GPIO to free resources
-// };
+function unexportOnClose() { //function to run when exiting program
+  buttonA.unexport(); // Unexport Button GPIO to free resources
+};
 
-// process.on('SIGINT', unexportOnClose); //function to run when user closes using
+process.on('SIGINT', unexportOnClose); //function to run when user closes using
