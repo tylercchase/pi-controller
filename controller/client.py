8ffe56bf57a4c77c22837e9e104500dd37d97ddb
tylercchase <tylercchase@gmail.com>
2021-04-20 11:31:39 -0800

    Press buttons on desktop on button presses

--- controller/client.py
+++ controller/client.py
@@ -9,18 +9,17 @@ sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 buttonA = Button(14)
 joystickUp = DigitalInputDevice(15)
 joystickSide = DigitalInputDevice(18)
-# buttons = {'a': Button(14),b: Button(15), c: Button}
-def send_data(_caller, _value):
-    sock.sendto(json.dumps({"caller": _caller, "value": _value}).encode(), (UDP_IP, UDP_PORT))
+buttons = {'a': Button(14),b: Button(15), c: Button}
+def send_data(_caller):
+    sock.sendto(json.dumps({"caller": _caller}).encode(), (UDP_IP, UDP_PORT))
 
 while True:
     if buttonA.is_pressed:
         print("Pressed")
-        send_data('buttonA', 1)
+        send_data('buttonA')
     print('up+down')
     print(joystickUp.value)
     print('side-side')
     print(joystickSide.value)
-    # else:
-        # print("Released")
-    sleep(1)
+    send_data('buttonD',1)
+    sleep(0.5)
--- desktop/server.js
+++ desktop/server.js
@@ -1,41 +1,50 @@
-var udp = require('dgram');
+const udp = require('dgram')
 
-var server = udp.createSocket('udp4');
+const server = udp.createSocket('udp4')
+const robot = require('robotjs')
 
+server.on('error', function (error) {
+  console.log('Error: ' + error)
+  server.close()
+})
 
-
-server.on('error',function(error){
-  console.log('Error: ' + error);
-  server.close();
-});
-
-server.on('message',function(msg,info){
-  console.log('Data received from client : ' + msg.toString());
-  console.log('Received %d bytes from %s:%d\n',msg.length, info.address, info.port);
+server.on('message', function (msg, info) {
+  console.log('Data received from client : ' + msg.toString())
+  console.log('Received %d bytes from %s:%d\n', msg.length, info.address, info.port)
   msg = JSON.parse(msg)
-  switch(msg.caller) {
+  switch (msg.caller) {
+    case 'buttonW':
+      console.log('Button W pressed')
+      robot.keyTap('w')
     case 'buttonA':
       console.log('Button A pressed')
-
+      robot.keyTap('a')
+    case 'buttonS':
+      console.log('Button S pressed')
+      robot.keyTap('s')
+    case 'buttonD':
+      console.log('Button D pressed')
+      robot.keyTap('d')
+    // case 'buttonX':
+    //   console.log('Button X pressed')
+    //   robot.keyTap('space')
   }
+})
 
-});
+// emits when socket is ready and listening for datagram msgs
+server.on('listening', function () {
+  const address = server.address()
+  const port = address.port
+  const family = address.family
+  const ipaddr = address.address
+  console.log('Server is listening at port' + port)
+  console.log('Server ip :' + ipaddr)
+  console.log('Server is IP4/IP6 : ' + family)
+})
 
-//emits when socket is ready and listening for datagram msgs
-server.on('listening',function(){
-  var address = server.address();
-  var port = address.port;
-  var family = address.family;
-  var ipaddr = address.address;
-  console.log('Server is listening at port' + port);
-  console.log('Server ip :' + ipaddr);
-  console.log('Server is IP4/IP6 : ' + family);
-});
+// emits after the socket is closed using socket.close();
+server.on('close', function () {
+  console.log('Socket is closed !')
+})
 
-//emits after the socket is closed using socket.close();
-server.on('close',function(){
-  console.log('Socket is closed !');
-});
-
-server.bind(2222);
-
+server.bind(2222)
